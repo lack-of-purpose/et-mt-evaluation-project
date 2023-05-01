af99ffec121528ed2e134d8841cb64dd4e250848
lack-of-purpose <35802031@o365.cuni.cz>
2023-05-01 16:11:12 +0200

    working POC

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # et-mt-evaluation-project
 The frontend and backend for eye tracking MT evalution project (without actiual eye tracking). 
+
+sample.html - just an example front for the server (chatGPT)
+
+index.html - homepage, run on localhost:8000
+
---
+++ webserv.py
@@ -0,0 +1,103 @@
+import http.server
+import socketserver
+import time
+import threading
+import json
+import traceback
+
+
+PORT = 8000
+# Define the handler for the web server
+class MyHandler(http.server.SimpleHTTPRequestHandler):
+    timestamps = []
+    problematic_words = []
+    #def do_PUT(self):
+        
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html; charset=utf-8")
+        self.end_headers()
+        super().do_GET()
+    
+    def do_POST(self):
+        if self.path == '/start':
+            # handle '/start' request
+            content_length = int(self.headers['Content-Length'])
+            post_data = self.rfile.read(content_length)
+            # write data to file
+            with open('out.txt', 'w') as output:
+                output.write(post_data.decode('utf-8'))
+                threading.Thread(target=self.mock_track).start()
+            # send response
+        elif self.path == '/next':
+            # handle '/next' request
+            self.send_response(200)
+            self.send_header("Content-type", "text/html; charset=utf-8")
+            self.end_headers()
+            content_length = int(self.headers['Content-Length'])
+            body = self.rfile.read(content_length)
+            data = body.decode('utf-8')
+            words = json.loads(data)
+            self.problematic_words.append(words)
+            print('Next clicked!')
+            # add additional data to file
+            ts = time.time()
+            self.timestamps.append(f'Start trial {ts}')
+        elif self.path == '/choice1':
+            print('choice1 clicked!')
+            ts = time.time()
+            self.timestamps.append(f'End trial {ts}')
+        elif self.path == '/choice2':
+            #for line in traceback.format_stack():
+            #    print(line.strip())
+            print('choice2 clicked!')
+            ts = time.time()
+            self.timestamps.append(f'End trial {ts}')
+        elif self.path == '/end':
+            self.send_response(200)
+            self.send_header("Content-type", "text/html; charset=utf-8")
+            self.end_headers()
+            content_length = int(self.headers['Content-Length'])
+            body = self.rfile.read(content_length)
+            data = body.decode('utf-8')
+            words = json.loads(data)
+            self.problematic_words.append(words)
+            print('End clicked!')
+            timestamps = open('timestamps.txt', 'w')
+            for item in self.timestamps:
+                timestamps.write(item)
+                timestamps.write('\n')
+            clicked_words = open('clicked_words.txt', 'w', encoding='utf-8')
+            trial_num = 1
+            for item in self.problematic_words:
+                clicked_words.write(str(trial_num))
+                clicked_words.write('\n')
+                for word in item:
+                    clicked_words.write(word)
+                    clicked_words.write(' ')
+                clicked_words.write('\n')
+                trial_num += 1
+        self.send_response(200)
+        self.send_header("Content-type", "text/html; charset=utf-8")
+        self.end_headers()
+    
+    def mock_track(self):
+        while 1:
+            with open('out.txt', 'a') as output:
+                ts = time.time()
+                output.write(str(ts))
+                output.write(" coordinates\n")
+            #output.write('coordinates\n')
+            time.sleep(1 / 60)
+
+# Create the web server
+with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
+    print("serving at port", PORT)
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        #pass
+        httpd.server_close()
+    # Serve the web pages indefinitely
+    #httpd.serve_forever()
